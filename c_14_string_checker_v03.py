98aa35949a0beed691802da87679db9f0edf0a2e
bassonk3767 <145512278+bassonk3767@users.noreply.github.com>
2024-03-05 13:31:54 +1300

    Almost complete currency component

---
+++ c_14_string_checker_v03.py
@@ -0,0 +1,34 @@
+# Functions...
+
+# Checks that users enter a valid response (e.g yes / no
+# Cash / credit) based on a list of options
+def string_checker(question, num_letters, valid_response):
+
+    # Error message
+    error = f"\nPlease choose {valid_response[0]} or {valid_response[1]}"
+
+    while True:
+
+        # User's response to the question
+        response = input(question).lower()
+
+        # Looks for matches in the user's
+        # response and the desired response
+        for item in valid_response:
+            if response == item[:num_letters] or response == item:
+                return item
+
+        # Prints message if there were no matches found.
+        print(error)
+
+
+# Main routine...
+
+# Options listed
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
+
+for case in range(0, 5):
+    want_instructions = string_checker("\nDo you want to read the"
+                                       "instructions?", 1, yes_no_list)
+    print(f"You chose {want_instructions}")
